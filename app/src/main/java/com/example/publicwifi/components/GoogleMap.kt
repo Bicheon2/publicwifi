a0f783a3d4254436fcc2e013fb50c7324a4a6fd6
Jinhjlog <dndn2615@gmail.com>
2023-10-14 03:35:37 +0900

    custom marker 마무리

--- app/src/main/java/com/example/publicwifi/components/GoogleMap.kt
+++ app/src/main/java/com/example/publicwifi/components/GoogleMap.kt
@@ -15,15 +15,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SignalWifi4Bar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import com.example.publicwifi.screens.WifiMapBox
+import com.example.publicwifi.screens.WifiNearbySearchButton
+import com.example.publicwifi.ui.theme.Primary
+import com.example.publicwifi.ui.theme.Secondary
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
@@ -31,7 +42,6 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerInfoWindow
-import com.google.maps.android.compose.MarkerInfoWindowContent
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
@@ -55,6 +65,7 @@ val ARRAY_INFO: Array<String> = arrayOf(
 
     )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapComponent(
     startLatLng: LatLng = LatLng(GWANGJU_LAT, GWANGJU_LON),
@@ -73,94 +84,63 @@ fun MapComponent(
             state = MarkerState(position = startLatLng),
             title = "현재 위치",
         )
-        if(latLngList.isNotEmpty()) {
+        if (latLngList.isNotEmpty()) {
             latLngList.forEachIndexed { index, latLng ->
 
                 MarkerInfoWindow(
                     state = MarkerState(position = latLng),
                     icon = bitmapDescriptorFromVector(
                         context = context,
-                        vectorResourceId = com.example.publicwifi.R.drawable.profile,
+                        vectorResourceId = com.example.publicwifi.R.drawable.wifi_svgrepo_com,
                         width = 100,
                         height = 100,
                     ),
                 ) { marker ->
-                Box(
-                    modifier = Modifier
-                        .padding(horizontal = 60.dp)
-                        .background(
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            shape = RoundedCornerShape(35.dp)
-                        )
-                ){
-                    Column(
-                        modifier = Modifier.padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                    Scaffold(
+                        topBar = {
+                            Box(
+                                modifier = Modifier
+                                    .height(25.dp)
+                                    .fillMaxWidth()
+                                    .background(
+                                        brush = Brush.horizontalGradient(
+                                            listOf(
+                                                Secondary,
+                                                Primary
+                                            )
+                                        )
+                                    )
+                            )
+                            {
+                                Text(
+                                    text = "Public Wifi",
+                                    modifier = Modifier.align(Alignment.Center),
+                                    fontSize = 18.sp,
+                                )
+                            }
+                        },
+                        modifier = Modifier.padding(horizontal = 60.dp).height(100.dp),
                     ) {
-
-//                        Image(
-//                            painter = painterResource(id = R.drawable.map),
-//                            contentDescription = null,
-//                            contentScale = ContentScale.Fit,
-//                            modifier = Modifier
-//                                .height(80.dp)
-//                                .fillMaxWidth(),
-//
-//                            )
-                        //.........................Spacer
-                        Spacer(modifier = Modifier.height(24.dp))
-                        //.........................Text: title
-                        Text(
-                            text = "Marker Title",
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier
-                                .padding(top = 10.dp)
-                                .fillMaxWidth(),
-                            style = MaterialTheme.typography.headlineSmall,
-                            color = MaterialTheme.colorScheme.primary,
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        //.........................Text : description
-                        Text(
-                            text = "Customizing a marker's info window",
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier
-                                .padding(top = 10.dp, start = 25.dp, end = 25.dp)
-                                .fillMaxWidth(),
-                            style = MaterialTheme.typography.bodyLarge,
-                            color = MaterialTheme.colorScheme.primary,
-                        )
-                        //.........................Spacer
-                        Spacer(modifier = Modifier.height(24.dp))
-
+                        Column(
+                            modifier = Modifier.padding(it)
+                        ) {
+                            Text(
+                                text = "Customizing a marker's info window",
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier
+                                    .padding(top = 10.dp, start = 25.dp, end = 25.dp)
+                                    .fillMaxWidth(),
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = MaterialTheme.colorScheme.primary,
+                            )
+                        }
                     }
                 }
-
-                }
             }
-//            MarkerInfoWindowContent(
-//                state = MarkerState(position = latLngList[0]),
-//            )
         }
     }
 }
 
-
-//fun bitmapDescriptorFromVector(
-//    context: Context,
-//    @DrawableRes vectorResourceId: Int
-//): BitmapDescriptor {
-//    val vectorDrawable: Drawable? = ContextCompat.getDrawable(context, vectorResourceId)
-//    vectorDrawable?.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
-//    val bitmap: Bitmap = Bitmap.createBitmap(
-//        vectorDrawable?.intrinsicWidth ?: 0,
-//        vectorDrawable?.intrinsicHeight ?: 0,
-//        Bitmap.Config.ARGB_8888
-//    )
-//    val canvas = Canvas(bitmap)
-//    vectorDrawable?.draw(canvas)
-//    return BitmapDescriptorFactory.fromBitmap(bitmap)
-//}
 
 fun bitmapDescriptorFromVector(
     context: Context,
